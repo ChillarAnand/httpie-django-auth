9afa4fc82d6c058af6a0b539943577e555f06ab7
chillaranand <anand21nanda@gmail.com>
2017-04-20 21:40:56 +0530

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as fh:
 
 setup(
     name='httpie-django-auth',
-    version='0.1.0',
+    version='0.1.1',
     description='django Auth plugin for HTTPie.',
     long_description=long_description,
 
